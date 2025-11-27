67b5f080ec9f0b3e71cd3c3a31b5b71799c7a111
kirill.avtonomov <kirill.avtonomov@yandex.ru>
2025-11-27 14:27:55 +0300

    убрал лишний ParentId

--- src/services/new/structure-item/utils.ts
+++ src/services/new/structure-item/utils.ts
@@ -53,7 +53,6 @@ export const getWorkbooksQuery = ({ctx, trx}: ServiceArgs, args: GetWorkbooksQue
             WorkbookModelColumn.Description,
             raw('null as ??', 'parentId'),
             WorkbookModelColumn.ProjectId,
-            raw('null as ??', CollectionModelColumn.ParentId),
             WorkbookModelColumn.TenantId,
             WorkbookModelColumn.CreatedBy,
             WorkbookModelColumn.CreatedAt,
